aefacb424539391bc06c0acd21da29086bb9d5e2
web525 <192389590@qq.com>
2019-08-29 15:40:37 +0800

    getRepeatItems: get the repeated items between several Arrays

--- src/comm/js/array/index.js
+++ src/comm/js/array/index.js
@@ -12,3 +12,24 @@ const makeArray = (length)=>{
     // return Array.from({length:100}, (v,k) => k);
     // return Array.from(new Array(length)).map((item,index)=>index);
 }
+// 
+const hasRepeatKey = (arr1,arr2)=>{
+    const setLength = [...new Set([...arr1,...arr2])].length;
+    const sumLength = arr1.length + arr2.length;
+    return setLength < sumLength;
+}
+const getRepeatItems = function(){
+    const arr = [...arguments].reduce((sum,item)=>{
+        sum = [...sum,...item]
+        return sum;
+    },[])
+    let obj = {};
+    arr.map(item=>{
+        obj[item] = ++obj[item] || 0;
+    })
+    const repeatItems = Object.keys(obj).filter(item=>obj[item]>0);
+    return {
+        repeatCount: obj,
+        repeatItems,
+    }
+}
